f3c493e711e35ac27d96425f2f84f9150675b0f7
cash <cash@cashwu.com>
2016-05-27 23:41:52 +0800

    init

---
+++ PrimeFactor.Test/PrimeFactorTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrimeFactor.Test
+{
+    [TestClass]
+    public class PrimeFactorTest
+    {
+        [TestMethod]
+        public void PrimeFactorTest_2()
+        {
+            var p = new PrimeFactor();
+
+            var expect = new []{2};
+            var result = p.GetPrimeFactor(2);
+
+            CollectionAssert.AreEqual(expect, result);
+        }
+
+        [TestMethod]
+        public void PrimeFactorTest_1()
+        {
+            var p = new PrimeFactor();
+
+            var expect = new int[0];
+            var result = p.GetPrimeFactor(1);
+
+            CollectionAssert.AreEqual(expect, result);
+        }
+
+
+    }
+}
---
+++ PrimeFactor/PrimeFactor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace PrimeFactor
+{
+    public class PrimeFactor
+    {
+        public int[] GetPrimeFactor(int num)
+        {
+            var list = new List<int>();
+
+            int o;
+            var r = GetPrimeFactor(num, 2, out o);
+            if (r)
+            {
+                list.Add(2);
+            }
+
+            if (o == 0)
+            {
+                return list.ToArray();
+            }
+
+            return list.ToArray();
+        }
+
+        private bool GetPrimeFactor(int num, int n, out int o)
+        {
+            if (num % n == 0)
+            {
+                o = num / n;
+                return true; ;
+            }
+
+            o = num;
+            return false;
+        }
+    }
+}
